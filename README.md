db57af9d40bb5223791fb57d51f8a8e4aa618a2f
lingdujun <yfzhang@hfut.edu.cn>
2024-03-19 17:44:49 +0800

    0.9

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Rebuild cif file to be compatible with Jems software
 ## How it works
 
 remove line numbers in "_space_group_symop_operation_xyz" part of cif files
+
+## How to use
+
+Just drop your cif to the file box, and click the buttom "rebuild cif", a new cif file with name "cif_filename-rebuild.cif" will be created at the same directory, where cif_filename is the name you droped into the box.
--- cif_rebuilder.py
+++ cif_rebuilder.py
@@ -1,25 +1,56 @@
-cif_file="D:\python\cif_rebuilder\W-bcc.cif"
-cif_data=[]
-# read_data
-reduilder_signal=0
-with open(cif_file) as raw_data:
-    lines=raw_data.readlines()
-    for i in lines:    
-        if reduilder_signal==1:
-            rebuild_i=i.split(' ')
-            del rebuild_i[0]
-            output_i=''
-            for j in rebuild_i:
-                output_i+=j
-            cif_data.append(output_i)
-        if reduilder_signal==0:
-            cif_data.append(i)
-        if '_space_group_symop_operation_xyz'in i:
-            reduilder_signal=1
-        elif 'loop_' in i:
-            reduilder_signal=0
- 
-with open(cif_file[0:-4]+'-rebuild'+'.cif','w') as file:
-    for i in cif_data:
-        file.writelines(i)
-print("Reduild Finished!")
+import tkinter as tk
+from tkinter import ttk
+import tkinterDnD 
+from tkinter import messagebox
+
+root = tkinterDnD.Tk()  
+root.title("Cif rebuilder")
+
+address_stringvar = tk.StringVar()
+address_stringvar.set('Drop cif here to rebuild!')
+
+def drop(event):
+    address_stringvar.set(event.data)
+    #cif_file=address_stringvar.get()
+
+def rebuild():
+    file=address_stringvar.get()
+    cif_data=[]
+    reduilder_signal=False
+    with open(file) as raw_data:
+        lines=raw_data.readlines()
+        for i in lines:    
+            if reduilder_signal==True:
+                rebuild_i=i.split(' ')
+                del rebuild_i[0]
+                output_i=''
+                for j in rebuild_i:
+                    output_i+=j
+                cif_data.append(output_i)
+            if reduilder_signal==False:
+                cif_data.append(i)
+            if '_space_group_symop_operation_xyz'in i:
+                reduilder_signal=True
+            elif 'loop_' in i:
+                reduilder_signal=False
+    with open(file[0:-4]+'-rebuild'+'.cif','w') as file:
+        for i in cif_data:
+            file.writelines(i)
+    messagebox.showinfo("Note","All Done! cif has been rebuilded.")      
+
+label_1 = ttk.Label(root, 
+                    ondrop=drop, 
+                    #ondragstart=drag_command,
+                    textvar=address_stringvar, 
+                    padding=50, 
+                    relief="solid")
+label_1.pack(fill="both", expand=True, padx=10, pady=10)
+
+button_1=tk.Button(root, 
+                   text="rebuild cif",
+                   command=rebuild,
+                   padx=10, 
+                   pady=10)
+button_1.pack()
+
+root.mainloop()
